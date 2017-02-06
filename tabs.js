7dcbc41b738244a955cdfbec015973bd573f9480
Wolfgang Marcos <wolfgang@alcateia.eti.br>
2017-02-06 11:40:15 -0400

    Adiciona Web Localization Dashboard, Guia de estilos, link do Github e tooltips! (#1)
    * Adiciona atalho ao Web Localization Dashboard
    * Adiciona tooltips aos itens do menu
    * Adiciona link para o Github, altera estilo aplicado no Web Localization Dashboard
    * Adiciona guia de estilos
    * Altera ícone da extensão, adiciona favicons para os itens da lista
    * Passa a usar favicons do Github e Cambridge armazenados internamente

--- tabs.js
+++ tabs.js
@@ -15,8 +15,20 @@ document.addEventListener("click", function(e) {
     browser.tabs.create({url: "https://wiki.mozilla.org/Brasil/L10n/Glossario"});
   }
 
+  else if (e.target.id === "tabs-guia-estilos") {
+    browser.tabs.create({url: "https://wiki.mozilla.org/L10n:Teams:pt-BR/Style_Guide"});
+  }
+
   else if (e.target.id === "tabs-cambridge") {
     browser.tabs.create({url: "http://dictionary.cambridge.org/pt/"});
+  }
+
+  else if (e.target.id === "tabs-web-localization-dashboard") {
+    browser.tabs.create({url: "https://l10n.mozilla-community.org/webdashboard/?locale=pt-BR"});
+  }
+
+  else if (e.target.id === "tabs-github") {
+    browser.tabs.create({url: "https://github.com/cynthiapereira/webextension-l10n-workflow"});
   }
 
   e.preventDefault();
