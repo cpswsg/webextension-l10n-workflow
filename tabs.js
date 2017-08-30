e899b72df78f008d3cfc323cf5e6c090d18a4a0e
Michal Stanke <michal@stanke.cz>
2017-08-30 14:31:29 +0200

    Use locale option in tabs.js

--- tabs.js
+++ tabs.js
@@ -1,6 +1,20 @@
+var locale;
+
+var localeOptionKey = 'options.locale';
+browser.storage.local.get(localeOptionKey).then(function(item) {
+  if (item[localeOptionKey]) {
+    locale = item[localeOptionKey];
+  } else {
+    locale = 'pt-BR';
+  }
+  if (locale != 'pt-BR') {
+    document.getElementById('tabs-glossario').parentElement.style.display = 'none';
+  }
+});
+
 document.addEventListener("click", function(e) {
   if (e.target.id === "tabs-pontoon") {
-    browser.tabs.create({ url: "https://pontoon.mozilla.org/pt-BR/" });
+    browser.tabs.create({ url: `https://pontoon.mozilla.org/${locale}/` });
   }
 
   else if (e.target.id === "tabs-pontoon-terminology") {
@@ -16,15 +30,15 @@ document.addEventListener("click", function(e) {
   }
 
   else if (e.target.id === "tabs-guia-estilos") {
-    browser.tabs.create({url: "https://wiki.mozilla.org/L10n:Teams:pt-BR/Style_Guide"});
+    browser.tabs.create({url: `https://mozilla-l10n.github.io/styleguides/${locale}/`});
   }
 
   else if (e.target.id === "tabs-cambridge") {
-    browser.tabs.create({url: "http://dictionary.cambridge.org/pt/"});
+    browser.tabs.create({url: `http://dictionary.cambridge.org/${locale.split('-')[0]}/`});
   }
 
   else if (e.target.id === "tabs-web-localization-dashboard") {
-    browser.tabs.create({url: "https://l10n.mozilla-community.org/webdashboard/?locale=pt-BR"});
+    browser.tabs.create({url: `https://l10n.mozilla-community.org/webdashboard/?locale=${locale}`});
   }
 
   else if (e.target.id === "tabs-github") {
